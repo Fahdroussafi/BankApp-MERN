67402cb9c2b621661e4d564cae53b4a6de0a4a70
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-02 14:37:18 +0100

    api auth route

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const colors = require("colors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const connectDB = require("./config/database");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 connectDB();
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
