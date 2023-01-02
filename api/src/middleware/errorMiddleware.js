36ccffa9e5482711269f2fb298e6e97211515ec0
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-02 14:37:26 +0100

    error handler

---
+++ api/src/middleware/errorMiddleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    error: error.message,
+    stack: error.stack,
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
