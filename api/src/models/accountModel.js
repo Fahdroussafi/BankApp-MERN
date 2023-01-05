b1a3d641b55013dab0ef663e79ad4484de405252
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-05 11:06:19 +0100

    Create accountModel.js

---
+++ api/src/models/accountModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: [true, "Please fill the information"],
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("accounts", accountSchema);
