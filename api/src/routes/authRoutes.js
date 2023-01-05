1ee33debdc0053f7361816086824a7b6d3fca8d0
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-05 14:54:54 +0100

    added get user by id route

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express();
-const { CreateUser, LoginUser } = require("../Controllers/authController");
+const {
+  CreateUser,
+  LoginUser,
+  GetUserById,
+} = require("../Controllers/authController");
 
 router.post("/create-user", CreateUser);
 router.post("/login", LoginUser);
+router.get("/get-user-by-id/:id", GetUserById);
 
 module.exports = router;
