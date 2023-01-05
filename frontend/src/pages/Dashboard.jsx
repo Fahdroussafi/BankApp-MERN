46145acf1c0e9ac84f5c4968d4a0953af88b6c74
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-05 16:41:49 +0100

    show user name in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import AccountForm from "../components/AccountForm";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Account Dashboard</p>
+      </section>
+      <AccountForm />
+    </>
+  );
 }
 
 export default Dashboard;
