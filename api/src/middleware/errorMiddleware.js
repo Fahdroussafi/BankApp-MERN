82846e3c68576a13bd6495fa6056d7069634ac5e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-05 11:06:13 +0100

    minor modification error middleware

--- api/src/middleware/errorMiddleware.js
+++ api/src/middleware/errorMiddleware.js
@@ -1,9 +1,9 @@
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
-    error: error.message,
-    stack: error.stack,
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
