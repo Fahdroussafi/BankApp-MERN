3ef4112471908e4da348ce9fed4e576ef5bc88cc
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-09 17:29:35 +0100

    get all accounts route

--- api/src/routes/accountRoutes.js
+++ api/src/routes/accountRoutes.js
@@ -5,11 +5,13 @@ const {
   setAccount,
   updateAccount,
   deleteAccount,
+  getAllAccounts,
 } = require("../controllers/accountController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/create-account", protect, setAccount);
 router.get("/get-accounts", protect, getAccounts);
+router.get("/get-all-accounts", getAllAccounts);
 router.delete("/delete-account/:id", protect, deleteAccount);
 router.put("/update-account/:id", protect, updateAccount);
 
