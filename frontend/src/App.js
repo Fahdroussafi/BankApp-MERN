a9f0bfc076c0c7c7b38ef26ad0fecbf59c017561
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:19:52 +0100

    admin dash route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import DashboardAdmin from "./pages/admin/DashboardAdmin";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-          </Routes> 
+            <Route path="/dashboard" element={<DashboardAdmin />} />
+          </Routes>
         </div>
       </Router>
       <ToastContainer />
