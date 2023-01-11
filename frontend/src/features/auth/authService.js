91540ec39485fb7fa9d11efcc917c0fbd4a0252c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:22:24 +0100

    localstorage minor changes

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -4,6 +4,7 @@ const API_URL = "/api/auth/";
 
 // Register user
 const register = async (userData) => {
+  const date = new Date();
   const response = await axios.post(
     "http://localhost:8080/api/auth/create-user",
     userData
@@ -11,6 +12,8 @@ const register = async (userData) => {
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("token_date", date.getTime());
   }
 
   return response.data;
@@ -18,10 +21,13 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
+  const date = new Date();
   const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("token_date", date.getTime());
   }
 
   return response.data;
