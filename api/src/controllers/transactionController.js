026b78642c05c752f084597adb042e72354ebeb1
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:19:17 +0100

    create transaction type logic fix

--- api/src/controllers/transactionController.js
+++ api/src/controllers/transactionController.js
@@ -1,54 +1,66 @@
 const asyncHandler = require("express-async-handler");
 const Account = require("../models/accountModel");
-const User = require("../models/userModel");
 const Transaction = require("../models/transactionModel");
 
-// create a new transaction for a user
-// POST /api/transactions
+// @desc    Create a new transaction
+// @route   POST /api/transactions
+// @access  Private
 
 const createTransaction = asyncHandler(async (req, res) => {
-  const { account_id, transaction_amount, transaction_type } = req.body;
+  const { account_id, transaction_amount, transaction_type, transfer_to } =
+    req.body;
 
   const account = await Account.findById(account_id);
 
-  if (!account) {
-    res.status(400);
+  if (account) {
+    if (transaction_type === "deposit") {
+      account.balance =
+        parseInt(account.balance) + parseInt(transaction_amount);
+    } else if (transaction_type === "withdrawal") {
+      account.balance =
+        parseInt(account.balance) - parseInt(transaction_amount);
+      if (account.balance < 0) {
+        res.status(400);
+        throw new Error("Insufficient funds");
+      }
+    } else if (transaction_type === "transfer") {
+      account.balance =
+        parseInt(account.balance) - parseInt(transaction_amount);
+
+      if (account.balance < 0) {
+        res.status(400);
+        throw new Error("Insufficient funds");
+      }
+
+      const transferToAccount = await Account.findById(req.body.transfer_to);
+      if (transferToAccount) {
+        transferToAccount.balance =
+          parseInt(transferToAccount.balance) + parseInt(transaction_amount);
+        await transferToAccount.save();
+      } else {
+        res.status(404);
+        throw new Error("Account not found");
+      }
+    }
+
+    await account.save();
+
+    const transaction = new Transaction({
+      user_id: req.user.id,
+      account_id,
+      transaction_amount,
+      transaction_type,
+      transfer_to,
+      balance: account.balance,
+    });
+
+    const createdTransaction = await transaction.save();
+
+    res.status(201).json(createdTransaction);
+  } else {
+    res.status(404);
     throw new Error("Account not found");
   }
-
-  const user = await User.findById(req.user.id);
-
-  //Check for User
-  if (!user) {
-    res.status(401);
-    throw new Error("User not found");
-  }
-
-  const transaction = await Transaction.create({
-    user_id: req.user.id,
-    account_id: account_id,
-    transaction_date: Date.now(),
-    transaction_amount: transaction_amount,
-    transaction_type: transaction_type,
-    balance: account.amount,
-  });
-
-  if (transaction_type === "deposit") {
-    account.amount = parseInt(account.amount) + parseInt(transaction_amount);
-  } else if (transaction_type === "withdrawal") {
-    account.amount = parseInt(account.amount) - parseInt(transaction_amount);
-  }
-  balance = account.amount;
-
-  // if the withdrawal amount is greater than the account balance, throw an error
-  if (account.amount < 0) {
-    res.status(400);
-    throw new Error("Insufficient funds");
-  }
-
-  await account.save();
-
-  res.status(200).json(transaction);
 });
 
 // get all transactions for a user
