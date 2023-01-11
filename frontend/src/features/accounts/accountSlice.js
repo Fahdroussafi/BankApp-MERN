e71b0a172aae173026d90d9b71873b7576b7bc8e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:21:54 +0100

    get accounts/delete accounts slice/extra reducers

--- frontend/src/features/accounts/accountSlice.js
+++ frontend/src/features/accounts/accountSlice.js
@@ -33,43 +33,45 @@ export const createAccount = createAsyncThunk(
   }
 );
 
-// deposit
-// export const deposit = createAsyncThunk(
-//   "accounts/deposit",
-//   async (data, thunkAPI) => {
-//     try {
-//       const token = thunkAPI.getState().auth.user.token;
-//       return await accountService.deposit(data, token);
-//     } catch (error) {
-//       const message =
-//         (error.response &&
-//           error.response.data &&
-//           error.response.data.message) ||
-//         error.message ||
-//         error.toString();
-//       return thunkAPI.rejectWithValue(message);
-//     }
-//   }
-// );
+// Get user account
+export const getAccounts = createAsyncThunk(
+  "accounts/get",
+  async (_, thunkAPI) => {
+    try {
+      // get token from state in the local storage
+      const token = thunkAPI.getState().auth.user.token;
 
-// withdraw
-// export const withdraw = createAsyncThunk(
-//   "accounts/withdraw",
-//   async (data, thunkAPI) => {
-//     try {
-//       const token = thunkAPI.getState().auth.user.token;
-//       return await accountService.withdraw(data, token);
-//     } catch (error) {
-//       const message =
-//         (error.response &&
-//           error.response.data &&
-//           error.response.data.message) ||
-//         error.message ||
-//         error.toString();
-//       return thunkAPI.rejectWithValue(message);
-//     }
-//   }
-// );
+      return await accountService.getAccount(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Delete user account
+export const deleteAccount = createAsyncThunk(
+  "accounts/delete",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await accountService.deleteAccount(id, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const accountSlice = createSlice({
   name: "account",
@@ -79,7 +81,7 @@ export const accountSlice = createSlice({
       state.isLoading = false;
       state.isSuccess = false;
       state.isError = false;
-      state.message = "";
+      state.message = null;
     },
   },
   extraReducers: (builder) => {
@@ -94,6 +96,34 @@ export const accountSlice = createSlice({
         state.accounts.push(action.payload);
       })
       .addCase(createAccount.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getAccounts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAccounts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.accounts = action.payload;
+      })
+      .addCase(getAccounts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(deleteAccount.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteAccount.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.accounts = state.accounts.filter(
+          (account) => account._id !== action.payload.id
+        );
+      })
+      .addCase(deleteAccount.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
