c72f262a63119e258fa7251fdcadccbba606dc91
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:22:13 +0100

    get account & delete account service done

--- frontend/src/features/accounts/accountService.js
+++ frontend/src/features/accounts/accountService.js
@@ -18,8 +18,38 @@ const createAccount = async (accountData, token) => {
   return response.data;
 };
 
+// Get account
+const getAccount = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL + "get-accounts", config);
+  return response.data;
+};
+
+// Delete user account
+const deleteAccount = async (accountId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.delete(
+    API_URL + "/delete-account/" + accountId,
+    config
+  );
+
+  return response.data;
+};
+
 const accountService = {
   createAccount,
+  getAccount,
+  deleteAccount,
 };
 
 export default accountService;
