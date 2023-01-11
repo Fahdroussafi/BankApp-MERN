5780fc4f05704f819ad85c3103e156f9819f2f51
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-11 17:23:07 +0100

    show accounts & token expire fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,48 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import AccountForm from "../components/AccountForm";
+import { getAccounts, reset } from "../features/accounts/accountSlice";
+import Spinner from "../components/Spinner";
+import AccountItem from "../components/AccountItem";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { accounts, isLoading, isError, message } = useSelector(
+    (state) => state.accounts
+  );
 
   useEffect(() => {
     if (!user) {
       navigate("/login");
+    } else {
+      dispatch(getAccounts());
     }
-  }, [user, navigate]);
+
+    const comparetime = () => {
+      const time = localStorage.getItem("token_date");
+      const currenttime = new Date().getTime();
+      if (currenttime - time > 3600000) {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        localStorage.removeItem("token_date");
+        window.location.reload();
+      }
+    };
+
+    comparetime();
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -21,6 +51,18 @@ function Dashboard() {
         <p>Account Dashboard</p>
       </section>
       <AccountForm />
+
+      <section className="content">
+        {accounts.length > 0 ? (
+          <div className="goals">
+            {accounts.map((account) => (
+              <AccountItem key={account._id} account={account} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any accounts </h3>
+        )}
+      </section>
     </>
   );
 }
