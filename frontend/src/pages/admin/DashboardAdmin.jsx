4fdd82c1fff16ddf74d7a1065f06f705b6fb4e09
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-15 02:30:30 +0100

    Update DashboardAdmin.jsx

--- frontend/src/pages/admin/DashboardAdmin.jsx
+++ frontend/src/pages/admin/DashboardAdmin.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import axios from "axios";
 import moment from "moment";
+import { Helmet } from "react-helmet";
 
 function DashboardAdmin() {
   const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
 
   const { user } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!user) {
-      navigate("/login");
+      window.location.href = "/login";
+    }
+    if (user && user.isAdmin === false) {
+      window.location.href = "/";
     }
     const fetchUsers = async () => {
       const config = {
@@ -29,40 +31,46 @@ function DashboardAdmin() {
     };
 
     fetchUsers();
-  }, []);
+  }, [user]);
 
   return (
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <th scope="col" className="px-6 py-3">
-            Name
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Email
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Created At
-          </th>
-        </thead>
-        <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-70">
-          {users.map((user) => (
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {user.name}
-              </th>
-              <td className="px-6 py-4">{user.email}</td>
-              <td className="px-6 py-4">
-                {moment(user.createdAt).format("DD/MM/YYYY")}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Helmet>
+        <title>Dashboard Admin</title>
+      </Helmet>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Created At
+            </th>
+          </thead>
+          <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-70">
+            {users.map((user) => (
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {user.name}
+                </th>
+                <td className="px-6 py-4">{user.email}</td>
+                <td className="px-6 py-4">
+                  {moment(user.createdAt).format("DD/MM/YYYY")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
